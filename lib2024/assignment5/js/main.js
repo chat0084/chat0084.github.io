b14f3d6798f5659ec71730a1baca002d0d2f8775
chat0084 <122629430+chat0084@users.noreply.github.com>
2023-04-14 17:34:44 -0400

    Final Assignment

---
+++ lib2024/assignment5/js/main.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// treats
+function myFunction() {
+  alert("Thank you but I am on a diet! :3");
+}
