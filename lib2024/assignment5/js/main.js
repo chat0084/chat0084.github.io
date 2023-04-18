0e25db8cd0533bc5be01632de441570041c4c808
chat0084 <122629430+chat0084@users.noreply.github.com>
2023-04-17 21:06:01 -0400

    test

--- lib2024/assignment5/js/main.js
+++ lib2024/assignment5/js/main.js
@@ -1,4 +1,13 @@
-// alert when given treats
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// treats
 function myFunction() {
   alert("Thank you :3");
 }
