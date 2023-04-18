d689c90bd3549d6df7ade9e27d2a33be257101ca
chat0084 <122629430+chat0084@users.noreply.github.com>
2023-04-17 21:45:50 -0400

    Final
    Validated

--- lib2024/finalassignment/js/main.js
+++ lib2024/finalassignment/js/main.js
@@ -1,12 +1,3 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // treats
 function myFunction() {
   alert("Thank you :3");
